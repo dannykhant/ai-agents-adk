4d07aeb9bd21de6852872d756a210837d5c26212
dannykhant <dannypmkhant@gmail.com>
2026-03-24 11:02:10 +0000

    feat: added google search agent

---
+++ personal_assistant/custom_agents.py
@@ -0,0 +1,10 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+
+google_search_agent = Agent(
+    model='gemini-2.5-flash',
+    name='google_search_agent',
+    description='A search agent that uses google search to get latest information about current events, weather, or business hours.',
+    instruction='Use google search to answer user questions about real-time, logistical information.',
+    tools=[google_search]
+)
